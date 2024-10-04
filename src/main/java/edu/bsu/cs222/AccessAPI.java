f18a4dc01275529dd617eccd18ca7268bcc48a1f
Josep <joseph.culp2005@gmail.com>
2024-10-04 13:30:39 -0400

    get URL connection and input stream

--- src/main/java/edu/bsu/cs222/AccessAPI.java
+++ src/main/java/edu/bsu/cs222/AccessAPI.java
@@ -1,5 +1,24 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.nio.charset.Charset;
+
 public class AccessAPI {
 
+    public URLConnection connectToGeocode(String address) throws IOException {
+        String encodedUrlString = "https://geocode.maps.co/search?q=" +
+                URLEncoder.encode(address, Charset.defaultCharset()) +
+                "&api_key=67001a89919b8736625306gta626488";
+        URL url = URI.create(encodedUrlString).toURL();
+        URLConnection connection = url.openConnection();
+        connection.connect();
+        return connection;
+    }
+
+
+    public InputStream getInputStream(URLConnection connection) throws IOException {
+        return connection.getInputStream();
+    }
 }
--- src/test/java/edu/bsu/cs222/AccessAPITest.java
+++ src/test/java/edu/bsu/cs222/AccessAPITest.java
@@ -1,5 +1,32 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
 public class AccessAPITest {
+
+    @Test
+    public void urlReturnTest() throws IOException {
+        AccessAPI api = new AccessAPI();
+        String address = "146 S Nursery Rd Anderson IN 46012 US";
+        URLConnection result;
+        result = api.connectToGeocode(address);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void inputStreamTest() throws IOException {
+        AccessAPI api = new AccessAPI();
+        String address = "146 S Nursery Rd Anderson IN 46012 US";
+        URLConnection connection;
+        connection = api.connectToGeocode(address);
+        InputStream result;
+        result = api.getInputStream(connection);
+        Assertions.assertNotNull(result);
+    }
 
 }
