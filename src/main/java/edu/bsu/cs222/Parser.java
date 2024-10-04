5c11dc77a5f9ee3f63aca4042d52ac480f7fe2b7
Josep <joseph.culp2005@gmail.com>
2024-10-04 13:53:35 -0400

    update gradle dependencies. make Parser class and ParserTest class.

---
+++ src/main/java/edu/bsu/cs222/Parser.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222;
+
+import java.io.InputStream;
+import net.minidev.json.JSONArray;
+
+public class Parser {
+
+    public JSONArray makeJSONArray(InputStream testInputStream, String keyword) {
+        return null;
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/ParserTest.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import net.minidev.json.JSONArray;
+import java.io.InputStream;
+
+public class ParserTest {
+
+    @Test
+    public void makeJSONArrayTest() {
+        Parser parser = new Parser();
+        InputStream testInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        String keyword = "lat";
+        JSONArray jsonArray = parser.makeJSONArray(testInputStream, keyword);
+        Assertions.assertEquals("40.115843", jsonArray.getFirst());
+
+    }
+}
