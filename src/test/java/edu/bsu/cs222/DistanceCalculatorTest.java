b1009dd1e0e208f63a6a88b11a4810b663025da6
Josep <joseph.culp2005@gmail.com>
2024-10-10 10:11:11 -0400

    Add more to DistanceCalculatorTest and add another test class. Tests fail and errors prevail

--- src/test/java/edu/bsu/cs222/DistanceCalculatorTest.java
+++ src/test/java/edu/bsu/cs222/DistanceCalculatorTest.java
@@ -1,5 +1,14 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DistanceCalculatorTest {
 
+    @Test
+    public void checkIfDistanceIsAccurate() {
+        DistanceCalculator distanceCalculator = new DistanceCalculator();
+        double result = distanceCalculator.calculateDistanceKiloMeters();
+        Assertions.assertEquals(result,);
+    }
 }
