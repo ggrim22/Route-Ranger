579960a7cacf8347514c8883e93dbc56ae46bf0d
Josep <joseph.culp2005@gmail.com>
2024-10-10 10:36:41 -0400

    Add citation for distance calculator code

--- src/main/java/edu/bsu/cs222/DistanceCalculator.java
+++ src/main/java/edu/bsu/cs222/DistanceCalculator.java
@@ -3,6 +3,13 @@ package edu.bsu.cs222;
 public class DistanceCalculator {
     private static final int EARTH_RADIUS = 6378;
     public double calculateDistanceKiloMeters(double lat1, double lon1, double lat2, double lon2) {
+        /*
+         *    Title: Equirectangular Distance Approximation
+         *    Author: Harpal Singh
+         *    Date: 10/10/2024
+         *    Availability: https://www.baeldung.com/java-find-distance-between-points
+         *
+         */
         double lat1Rad = Math.toRadians(lat1);
         double lat2Rad = Math.toRadians(lat2);
         double lon1Rad = Math.toRadians(lon1);
