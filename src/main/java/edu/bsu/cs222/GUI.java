149bdf8de9236062fa79dacc6b6c0764ebdea627
Josep <joseph.culp2005@gmail.com>
2024-10-10 22:12:15 -0400

    adjust GUI spacing, not finished yet.

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -1,7 +1,9 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -12,13 +14,21 @@ import javafx.scene.control.TextField;
 public class GUI extends Application {
     private final Button getDistanceButton = new Button("Get Distance");
     private final TextField inputFirstAddress = new TextField();
+
     private final TextField inputSecondAddress = new TextField();
     private final TextField distanceField = new TextField();
+    private final HBox hBox1 = new HBox();
+    private final HBox hBox2 = new HBox();
 
     @Override
+
+
     public void start(Stage stage) {
+        inputFirstAddress.setPrefWidth(170);
+        inputSecondAddress.setPrefWidth(170);
         configure(stage);
     }
+
     private void configure(Stage stage) {
         stage.setTitle("Location Explorer");
         stage.setScene(new Scene(createRoot()));
@@ -27,18 +37,27 @@ public class GUI extends Application {
         stage.show();
     }
 
-    private Pane createRoot(){
+    private Pane createRoot() {
         VBox root = new VBox();
-        root.getChildren().addAll(
+        hBox1.getChildren().addAll(
                 new Label("First Address"),
                 inputFirstAddress,
                 new Label("Second Address"),
-                inputSecondAddress,
-                getDistanceButton,
-                new Label("Distance"),
-                distanceField);
+                inputSecondAddress);
+        hBox2.getChildren().addAll(
+                getDistanceButton
+        );
+        formatHBox(hBox1);
+        formatHBox(hBox2);
+        HBox hBox3 = new HBox();
+        hBox3.getChildren().addAll(new Label("Distance"), distanceField);
+        root.getChildren().addAll(hBox1, hBox2, hBox3);
         return root;
     }
 
+    private void formatHBox(HBox hBox) {
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(50);
+    }
 
 }
