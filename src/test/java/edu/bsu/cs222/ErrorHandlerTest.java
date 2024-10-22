f4c895875d28c1c71de938df83fd97c4b5ae90ba
EnragedGiraffe55 <karterwest13@gmail.com>
2024-10-22 15:08:15 -0400

    fix minor bugs

--- src/test/java/edu/bsu/cs222/ErrorHandlerTest.java
+++ src/test/java/edu/bsu/cs222/ErrorHandlerTest.java
@@ -1,11 +1,9 @@
 package edu.bsu.cs222;
 
-import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class ErrorHandlerTest {
 
