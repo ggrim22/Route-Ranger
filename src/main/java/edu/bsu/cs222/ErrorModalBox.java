b09d4aa1a7b11f069e0b10fa5e7a5135fd5736bd
Josep <joseph.culp2005@gmail.com>
2024-10-22 10:38:25 -0400

    add cancel button to error modal boxes.

--- src/main/java/edu/bsu/cs222/ErrorModalBox.java
+++ src/main/java/edu/bsu/cs222/ErrorModalBox.java
@@ -1,15 +1,23 @@
 package edu.bsu.cs222;
 
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 
 import java.io.IOException;
 
 public class ErrorModalBox {
 
+    private Dialog<String> configureErrorModalBox(){
+        Dialog<String> errorBox = new Dialog<>();
+        errorBox.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
+        errorBox.setHeight(111);
+        return errorBox;
+    }
+
     public void networkConnectionPopUp() {
         ErrorHandler errorHandler = new ErrorHandler();
         if(errorHandler.networkConnectionError()) {
-            Dialog<String> errorBox = new Dialog<>();
+            Dialog<String> errorBox = configureErrorModalBox();
             errorBox.setContentText("There has been a network error. Please try again later.");
             errorBox.show();
         }
@@ -18,18 +26,20 @@ public class ErrorModalBox {
     public void noInputFoundPopUp(String input) {
         ErrorHandler errorHandler = new ErrorHandler();
         if(errorHandler.noInputFoundError(input)) {
-            Dialog<String> errorBox = new Dialog<>();
+            Dialog<String> errorBox = configureErrorModalBox();
             errorBox.setContentText("There has been an error, please fill each alotted space.");
             errorBox.show();
+
         }
     }
 
     public void noAddressFoundPopUp(String keyword, String address) throws IOException {
         ErrorHandler errorHandler = new ErrorHandler();
         if(errorHandler.noAddressFoundError(keyword,address)) {
-            Dialog<String> errorBox = new Dialog<>();
+            Dialog<String> errorBox = configureErrorModalBox();
             errorBox.setContentText("There has been an error, please enter a valid address");
             errorBox.show();
+
         }
     }
 
