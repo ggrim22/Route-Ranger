83e7c6e6538414fe6a2092d326cca136fa21fb2e
EnragedGiraffe55 <karterwest13@gmail.com>
2024-10-31 11:15:11 -0400

    update url format in connection function

--- src/main/java/edu/bsu/cs222/AccessAPI.java
+++ src/main/java/edu/bsu/cs222/AccessAPI.java
@@ -23,7 +23,8 @@ public class AccessAPI {
                 "center=lonlat%3A" + URLEncoder.encode(lon, Charset.defaultCharset()) +
                 "%2C" + URLEncoder.encode(lat, Charset.defaultCharset()) +
                 "zoom=14.3497&" +
-                "marker=lonlat%3A-122.29188334609739%2C47.54403990655936" +
+                "marker=lonlat%3A" + URLEncoder.encode(lon, Charset.defaultCharset()) +
+                "%2C" + URLEncoder.encode(lat, Charset.defaultCharset()) +
                 "%3Btype%3Aawesome%3Bcolor%3A%23bb3f73%3Bsize%3Ax-large%3Bicon%3Apaw%7Clonlat%3A-122.29282631194182%2C47.549609195001494%3Btype%3Amaterial%3Bcolor%3A%234c905a%3Bicon%3Atree%3Bicontype%3Aawesome%7Clonlat%3A-122.28726954893025%2C47.541766557545884%3Btype%3Amaterial%3Bcolor%3A%234c905a%3Bicon%3Atree%3Bicontype%3Aawesome&" +
                 "apiKey=aa0bba8d694e473db0f91977c507a13a";
 
