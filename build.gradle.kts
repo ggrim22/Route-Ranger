9ec0299163b7d5985ba26b7d63876e37044e60a9
Josep <joseph.culp2005@gmail.com>
2024-11-05 09:58:48 -0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.slf4j:slf4j-nop:2.0.11")
+    implementation("org.slf4j:slf4j-nop:2.0.11")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
     implementation("net.minidev:json-smart:2.5.0")
     implementation("com.jayway.jsonpath", "json-path", "2.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
 tasks.test {
