882daa823a0e3493e61d355a9e8f62722a7de1c0
EnragedGiraffe55 <karterwest13@gmail.com>
2024-11-26 09:56:22 -0500

    Fix minor Bugs

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -24,7 +23,6 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static javax.swing.text.StyleConstants.setBackground;
 
 public class GUI extends Application {
     //Instance Variables
