cb12aab97bcf7030437c40a3d86765b983aebcd3
EnragedGiraffe55 <karterwest13@gmail.com>
2024-12-05 17:12:35 -0500

    Fix small bug in no input error

--- src/main/java/edu/bsu/cs222/ErrorTypeHandler.java
+++ src/main/java/edu/bsu/cs222/ErrorTypeHandler.java
@@ -9,7 +9,7 @@ public class ErrorTypeHandler {
 
     public boolean noInputFoundError(String address1, String address2) {
         if (address1 == null || address2 == null) return true; // Null check
-        return (address1.trim().isEmpty() && address2.trim().isEmpty()); // Trim and check
+        return (address1.trim().isEmpty() || address2.trim().isEmpty()); // Trim and check
     }
 
     public boolean noInputFoundError(String address) {
